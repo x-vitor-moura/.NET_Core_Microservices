02315a2f3f6d6a34369fb8c4007ee4f06a3941ff
Vitor Moura <44928381+x-vitor-moura@users.noreply.github.com>
2022-09-01 15:58:14 +0100

    Section 2: Product API basic setup

---
+++ MangoRestaurant/Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.ProductAPI.DbContexts.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
