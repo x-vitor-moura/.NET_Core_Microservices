8a994266d895b9862f629d8bf5940d245b96833a
Vitor Moura <44928381+x-vitor-moura@users.noreply.github.com>
2022-09-10 23:08:54 +0100

    Secction 7 Completed

--- MangoRestaurant/Mango.Web/Controllers/HomeController.cs
+++ MangoRestaurant/Mango.Web/Controllers/HomeController.cs
@@ -29,11 +29,10 @@ namespace Mango.Web.Controllers
         {
             List<ProductDto> list = new();
             var response = await _productService.GetAllProductsAsync<ResponseDto>("");
-            if (response != null && response.IsSuccess)
+            if(response!=null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
             }
-
             return View(list);
         }
 
@@ -41,12 +40,11 @@ namespace Mango.Web.Controllers
         public async Task<IActionResult> Details(int productId)
         {
             ProductDto model = new();
-            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId, "");
+            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId,"");
             if (response != null && response.IsSuccess)
             {
                 model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
             }
-
             return View(model);
         }
 
@@ -64,7 +62,7 @@ namespace Mango.Web.Controllers
         [Authorize]
         public async Task<IActionResult> Login()
         {
-
+            
             return RedirectToAction(nameof(Index));
         }
 
